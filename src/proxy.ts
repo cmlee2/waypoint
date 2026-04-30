3749d577271952a2d541f297866d692634733c8e
Cameron Lee <CAMERONMLEE.HOME@GMAIL.COM>
2026-04-29 21:19:42 -0500

    fix: bypass auth middleware for static public assets

--- src/proxy.ts
+++ src/proxy.ts
@@ -14,11 +14,10 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
+     * - _next (Next.js internals)
+     * - any request for a static asset with a file extension
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next|.*\\..*).*)',
     '/(api|trpc)(.*)',
   ],
 }
